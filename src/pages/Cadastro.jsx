f148170fee751d2fcb593b569b37e037201820d9
gabriel <gabriel@PC-RATO>
2025-05-03 15:27:53 -0300

    Remove unused firebaseConfig import

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom'; // Importando o useNavigate
-import { firebaseConfig } from '../firebase/config'; // Certifique-se de que está importando a configuração
+//import { firebaseConfig } from '../firebase/config'; // Certifique-se de que está importando a configuração
 
 const Cadastro = () => {
   const [email, setEmail] = useState('');
